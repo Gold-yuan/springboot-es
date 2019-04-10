a375b6791c0ad431a4b874545d5ded89bd1fcaea
Gold-yuan <515789026@qq.com>
2019-04-10 18:32:38 +0800

    end day

--- src/main/java/com/tsdp/demo/ElasticsearchRestClient.java
+++ src/main/java/com/tsdp/demo/ElasticsearchRestClient.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 /**
  * this file to you under the Apache License, version 2.0 (the "License"); you
